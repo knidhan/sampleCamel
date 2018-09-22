b7a7ff3e3269f4ac7e35b3797b3597161af66237
knidhan <knidhan@hotmail.com>
2018-09-22 19:01:17 +0530

    demo project

---
+++ src/main/java/com/myapp/SpringBootApplicationTest.java
@@ -0,0 +1,15 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationTest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationTest.class, args);
+		System.out.println("hello");
+
+	}
+
+}
